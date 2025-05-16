82e4abf6c86555fe410c019760a76d0a6778edf9
AhmedElsenosy <ahmedelsenosy487@gmail.com>
2025-05-16 18:50:05 +0300

    Products list from GET endpoint

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
 import Slider from './components/Slider';
+import ProductsList from './components/ProductsList';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Slider />
+      <ProductsList />
     </div>
   );
 }
